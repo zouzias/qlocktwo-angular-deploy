daf656382aa4acff19a490662220fdffe0df61da
Anastasios Zouzias <anastasios.zouzias@swisscom.com>
2015-12-23 18:08:03 +0100

    deploy qlocktwo app

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 var express = require('express');
+var http = require('http');
+var gzippo = require('gzippo');
+
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.use(gzippo.staticGzip('' + __dirname));
+app.use('/*', function(req, res){
+	res.sendfile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+var server = http.createServer(app);
+server.listen(process.env.PORT || 5000);
